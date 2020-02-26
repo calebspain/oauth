827c2de8f8ad93ff34085550b69301a8a82767cf
Caleb Spain <crspain@uh.edu>
2020-02-25 21:47:53 -0600

    Updated Port

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.listen(3000, () => {
-    console.log('app now listending for requests on port 3000')
+app.listen(5000, () => {
+    console.log('app now listending for requests on port 5000')
 })
